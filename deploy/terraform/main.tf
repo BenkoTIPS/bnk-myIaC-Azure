c1cb30b3ae89415b377817e5b855b4dde5e7246a
Mike Benkovich <mike@benko.com>
2022-01-21 12:10:43 -0600

    tweak template

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -52,7 +52,7 @@ resource "azurerm_resource_group" "rg" {
 
 
 resource "azurerm_app_service_plan" "plan" {
-  name                = "${var.app_name}-plan"
+  name                = "${var.env_name}-${var.app_name}-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   kind                = "linux"
